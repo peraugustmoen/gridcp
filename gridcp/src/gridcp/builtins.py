1c38e1ce7c56b612302aaead38ce4889a73c5dc6
peraugustmoen <peraugust.pa@gmail.com>
2026-03-11 16:20:48 +0100

    reformat!

--- gridcp/src/gridcp/builtins.py
+++ gridcp/src/gridcp/builtins.py
@@ -449,8 +449,9 @@ def penalty_regression_direct(pos, t, p):
 
     return rr
 
+
 @nb.njit(cache=True)
-def change_univariate_simple_f(sum_pre, sum_post, pos, t, q = 2):
+def change_univariate_simple_f(sum_pre, sum_post, pos, t, q=2):
     g = t - pos
     n1 = pos
     n2 = g
@@ -460,13 +461,14 @@ def change_univariate_simple_f(sum_pre, sum_post, pos, t, q = 2):
     out_stat = (mean_pre - mean_post) ** q
     return out_stat
 
+
 @nb.njit(cache=True)
 def change_univariate_simple_h(x):
     return x
+
 
 @nb.njit(cache=True)
 def change_univariate_simple_penalty(pos, t, p):
     logg = fastlog(t)
     logg = logg + math.sqrt(logg)
     return logg
-
